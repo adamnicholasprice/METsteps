c23a67768cfbbb82125b9a55d824a8810ac20b55
adamnicholasprice <adamnicholasprice@gmail.com>
2017-07-28 12:43:26 -0600

    Update shinyPlot_HUC_Taylor_Diagram.R

--- R/shinyPlot_HUC_Taylor_Diagram.R
+++ R/shinyPlot_HUC_Taylor_Diagram.R
@@ -19,7 +19,7 @@ shinyPlot_HUC_Taylor_Diagram <- function(default. = FALSE,
       plotrix::taylor.diagram(ref = subToHUC.[,1],
                               model = subToHUC.[,2],
                               col = cbPalette.[2],
-                              pos.cor=FALSE
+                              pos.cor=FALSE,
                               pcex = 2,
                               mar = c(5,7,5,7))
     }else{
@@ -31,7 +31,7 @@ shinyPlot_HUC_Taylor_Diagram <- function(default. = FALSE,
         plotrix::taylor.diagram(ref = subToHUC.[,1],
                                 model = subToHUC.[,i],
                                 add = TRUE,
-                                pos.cor=FALSE
+                                pos.cor=FALSE,
                                 col = cbPalette.[i],
                                 pcex = 2)
       }
